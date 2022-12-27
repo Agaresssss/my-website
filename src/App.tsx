d8ba17ffa685a5afbe4c659861135202644f83aa
Ball <ball.fiy555@gmail.com>
2022-12-27 17:37:43 +0700

    `fix` : fix import red line

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import {Route, Routes} from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
+import PageContainer from './components/PageContrainer'
 
 function App() {
   
