61f897b605c00fd474df0f18f4c0a065577ea5a1
Ball <ball.fiy555@gmail.com>
2023-01-20 14:37:44 +0700

    `feat` : data file

---
+++ src/data/PersonalData.ts
@@ -0,0 +1,101 @@
+export const PersonalData = {
+    introduction: {
+        intro: `HI, EVERYBODY`,
+        intro2: ` I'M PHUETTIPOL`,
+        occupation: "INTERNSHIP",
+    },
+    name: "Phuettipol Jitjaroenkit",
+    imagePath: "src/assets/DSCF1733_20.jpg",
+    dob: new Date(2001, 2, 14),
+    education: [
+        {
+            year: "2014-2019",
+            name: "Taweethapisek School",
+        },
+        {
+            year: "2020-2023",
+            name: "King Mongkut's University of Technology Thonburi",
+        },
+    ],
+    interests: [
+        {
+            icon: "src/assets/flaticon/001-desktop.svg",
+            title: "Computer",
+            detail:
+                "I'm interested in Computer, especially in hardware. I like to build my own computer.",
+        },
+        {
+            icon: "src/assets/flaticon/003-robot 1.svg",
+            title: "Technology",
+            detail: "I'm like to learn about new technology",
+        },
+        {
+            icon: "src/assets/flaticon/005-shuttlecock.svg",
+            title: "Sport",
+            detail: "I like to play sport, especially badminton with my friends",
+        },
+        {
+            icon: "src/assets/flaticon/004-adjustment 1.svg",
+            title: "Music",
+            detail:
+                "In a freetime. I will listen to music to relax and sometime I learn how to produce music from  youtube.",
+        },
+        {
+            icon: "src/assets/flaticon/002-camera 1.svg",
+            title: "Photography",
+            detail:
+                "I love take a picture of nature and people. I like to take a picture of sunset and sunrise.",
+        },
+    ],
+    experiences: [
+        {
+            img: "src/assets/portfolio-pic/modern-doc/logo.png",
+            title: "Modern doc",
+            description: "Modern Doc is a web application for documentary management. The user can create, adjust, or take notes then export and download it in pdf form.",
+            role : "My position in this project is to design the ui and to write a front end framework using react.",
+            path: "modern-doc",
+            pics: 9
+        },
+        {
+            img: "src/assets/portfolio-pic/scispec/logo1 3.png",
+            title: "ERP Scispec",
+            description: "Sci Spec is an application for internal data management.",
+            role :"My position in this project is to design the new database.",
+            path: "scispec",
+            pics: 1
+        },
+        {
+            img: "src/assets/portfolio-pic/angel-bank/log2.png",
+            title: "Angel bank",
+            description: "Angel Bank is an online banking application. The user can exchange currencies and make transactions.",
+            role :"My position in this project is to query the information from the database and send it to the front end framework using express.",
+            path: "angel-bank",
+            pics: 1
+        },
+    ],
+    contact: [
+        {
+            title: "Phone",
+            description: "+66 87 653 2059",
+        },
+        {
+            title: "Mail",
+            description: "phuettipol.jjrk@mail.kmutt.ac.th",
+        }, {
+            title: "Github",
+            description: "https://github.com/Agaresssss",
+        }, {
+            title: "Facebook",
+            description: "Ball Around",
+        }, {
+            title: "Instagram",
+            description: "Ball Around",
+        }, {
+            title: "Linkedin",
+            description: "www.linkedin.com/in/phuettipol-jjrk",
+        }
+    ],
+
+};
+
+
